73f8ed457b6ae52a8dbd4f5769c417db72c63399
Surapong <Tor@Surapongs-MacBook-Pro.local>
2017-03-12 22:25:46 +0700

    add file 02-13

---
+++ 05-active-links/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { render } from 'react-dom'
+import App from './modules/App'
+import About from './modules/About'
+import Repos from './modules/Repos'
+import { Router, Route, hashHistory } from 'react-router'
+
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={App}>
+      {/* make them children of `App` */}
+      <Route path="/repos" component={Repos}/>
+      <Route path="/about" component={About}/>
+    </Route>
+  </Router>
+
+), document.getElementById('app'))
+
---
+++ 09-index-links/modules/Repos.js
@@ -0,0 +1,20 @@
+// modules/Repos.js
+import React from 'react'
+import NavLink from './NavLink'
+
+export default React.createClass({
+  render() {
+    return (
+      <div>
+        <h2>Repos</h2>
+
+        {/* add some links */}
+        <ul>
+          <li><NavLink to="/repos/reactjs/react-router">React Router</NavLink></li>
+          <li><NavLink to="/repos/facebook/react">React</NavLink></li>
+        </ul>
+        {this.props.children}
+      </div>
+    )
+  }
+})
